9b72719f13aac255641d6febae0f968f63d362e2
Lawrence Ham <lham.staffsetter@gmail.com>
2019-12-13 12:46:00 -0700

    sync-13-12-01

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const fs = require('fs');
 const path = require('path');
 const AWS = require('aws-sdk');
 
+const mongoose = require('mongoose');
 const shortId = require('shortid');
+
+const V2T = require('./models/voice_to_text');
 
 let awsCreds = { 
     accessKeyId: process.env.AWS_ID, 
@@ -62,9 +65,29 @@ let transcribeUri = async (uri) => {
     });
 }
 
+async function createRecord(s3Uri, jobId) {
+    return new V2T({
+        s3_uri: s3Uri,
+        job_id: jobId,  
+    })
+    .save();
+}
+
+
 async function main() {
     let ret = null;
+
+    let connector = mongoose.connect(process.env.MONGODB_ATLAS,  { useNewUrlParser: true , useCreateIndex: true, useUnifiedTopology: true });
     
+    await connector.then(async () => {
+        console.log('Connected to MongoDB Atlas in the Cloud!');
+    })
+    .catch((reason) => {
+        console.log('Connection to MongoDB Atlas failed.');
+        console.log(reason);
+    })
+
+
     try {
         ret = await uploadFileToS3Bucket('./samples/vmail_sample1.wav');
     }
@@ -83,12 +106,24 @@ async function main() {
         console.log(e);
     }
 
-    console.log(ret);
+    let jobId = ret.TranscriptionJob.TranscriptionJobName;
+
+    if( !jobId ) {
+        console.log('Failed to obtain TranscriptionJobName...');
+    }
+    else {
+        let x = await createRecord(s3Uri, jobId);
+
+        mongoose.connection.close();
+
+        console.log(x);        
+    }
+
     // This does not return the transcription results.
     //
     // Lamba Function created (in AWS) to wait for a CloudWatch event (this must also be set up)
-    // when the transcription job is complete Transcribe issues event that triggers our Lambda
-    // function to callback to our server that the transcription is done (JobTitle included).
+    // When the transcription job is complete Transcribe issues an event that triggers our Lambda
+    // function to send a callback to our server that the transcription is done (JobTitle included).
     // Using JobTitle we can continue to use AWS SDK to look up that JobTitle/ID and get results
     // for storage in our waiting web-hook. -Lawrence
 }
---
+++ models/voice_to_text.js
@@ -0,0 +1,36 @@
+// lham 2019
+
+const mongoose = require('mongoose')
+
+const V2TSchema = new mongoose.Schema({
+    id: mongoose.SchemaTypes.ObjectId,
+
+    created_at: { 
+        type: Date, 
+        default: Date.now,
+        required: true 
+    },
+    job_id: {
+        type: String, 
+        required: true 
+    },
+    s3_uri: {
+        type: String,
+        required: true,
+    },
+    message_text: {
+        type: String,
+        default: null, 
+    },
+
+    /*
+    message_id: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'messages'
+    },
+    */
+
+})
+
+module.exports = mongoose.model('voice2text', V2TSchema);
+
