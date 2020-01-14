49cbc199fe1097b533c89fa10682d54ddeec4de0
Lawrence Ham <lham@staffsetter.com>
2020-01-14 08:17:52 -0800

    real world transition

--- index.js
+++ index.js
@@ -5,16 +5,33 @@ require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const AWS = require('aws-sdk');
-
+const commander = require('commander');
+const request = require("request-promise-native");
 const mongoose = require('mongoose');
 const shortId = require('shortid');
 
 const V2T = require('./models/voice_to_text');
 
+// set up command line processor and parse 
+commander.version('0.1.1', '-v, --version')
+    .usage('[OPTIONS]...')
+    .option('-d, --delay <ms>', 'Delay this script for n ms before starting...')
+    .requiredOption('-t, --target <url>', 'Specify URI of target .WAV file.')
+    .parse(process.argv);
+
+let targetUri = commander.target;
+
+if( !targetUri ) {
+    console.log('You must use the -t or --target <url> to specifiy where the .wav URL end-point.')
+    process.exit(1);
+}
+
+
+// set up our AWS credentials
 let awsCreds = { 
     accessKeyId: process.env.AWS_ID, 
     secretAccessKey: process.env.AWS_SECRET,
-    region: 'us-east-2' // required by TranscribeService -Lawrence
+    region: 'us-east-2' // required by TranscribeService 
 };
 
 const s3 = new AWS.S3(awsCreds);
@@ -22,14 +39,8 @@ const v2t = new AWS.TranscribeService(awsCreds);
 
 let uploadFileToS3Bucket = async (fileName) => {
     return new Promise((resolve, reject) => {
-        let fileBuffer = null;
 
-        try {
-            fileBuffer = fs.readFileSync(fileName);
-        }
-        catch(err) {
-            reject(err);
-        }
+        let fileBuffer = fs.readFileSync(fileName); // .catch will in promise will receive any read errors
 
         let params = {
             Bucket: process.env.AWS_S3_BUCKET,
@@ -48,7 +59,17 @@ let uploadFileToS3Bucket = async (fileName) => {
     });
 }
 
-let transcribeUri = async (uri) => {
+let downloadWav = async (wavUri, outputFilename) => {
+    console.log(`Downloading target WAV file: ${wavUri}...`);
+
+    let wavBuffer = await request.get({uri: wavUri, encoding: null});       
+                                                                            
+    console.log(`Saving wav download to ${outputFilename}`);
+                                                                            
+    fs.writeFileSync(outputFilename, wavBuffer);                                
+}
+
+let transcribeUri = async (uri, job_name) => {
     return new Promise((resolve, reject) => {
 
         let params = {
@@ -56,57 +77,57 @@ let transcribeUri = async (uri) => {
             Media: { 
               MediaFileUri: uri,
             },
-            TranscriptionJobName: shortId.generate(),
+            TranscriptionJobName: job_name, 
             MediaFormat: 'wav',
         }
 
         v2t.startTranscriptionJob(params, (err, data) => {
             return (err) ? reject(err) : resolve(data);
         });
-        
+
     });
 }
 
-async function createRecord(s3Uri, jobId) {
-    return new V2T({
-        s3_uri: s3Uri,
-        job_id: jobId,  
-    })
-    .save();
+async function createRecord(S3Uri, jobId) {
+    return new V2T({ s3_uri: S3Uri,job_id: jobId }).save();
 }
 
 
 async function main() {
     let ret = null;
-
+    let jobName = shortId.generate();
+    let fileName = jobName + '.wav';
     let connector = mongoose.connect(process.env.MONGODB_ATLAS,  { useNewUrlParser: true , useCreateIndex: true, useUnifiedTopology: true });
     
-    await connector.then(async () => {
-        console.log('Connected to MongoDB Atlas in the Cloud!');
-    })
-    .catch((reason) => {
-        console.log('Connection to MongoDB Atlas failed.');
-        console.log(reason);
-    })
+    await connector
+        .then(async () => {
+            console.log('Connected to MongoDB Atlas in the Cloud!');
+        })
+        .catch((reason) => {
+            console.log('Connection to MongoDB Atlas failed.');
+            console.log(reason);
+        })
 
 
-    try {
-        ret = await uploadFileToS3Bucket('./samples/vmail_sample1.wav');
-    }
-    catch(e) {
-        return console.log(e);
-    }
+    await downloadWav(targetUri, fileName)
+        .then(() => console.log('WAV download complete.'))
+        .catch((e) => {
+            console.trace(e);
+        })
 
-    let s3Uri = ret.Location;
+    let S3Uri = null;
 
-    console.log(`S3 successfully uploaded: ${s3Uri}`);
+    await uploadFileToS3Bucket(fileName)
+        .then((ret) => {
+            S3Uri = ret.Location;
+            console.log(`File moved to S3 bucket: ${S3Uri}`);
+        })
+        .catch((e) => {
+            console.trace(e);
+        })
 
-    try {
-        ret = await transcribeUri(s3Uri);
-    }
-    catch(e) {
-        console.log(e);
-    }
+    ret = await transcribeUri(S3Uri, jobName)
+                .catch((e) => { console.log(e) });
 
     let jobId = ret.TranscriptionJob.TranscriptionJobName;
 
@@ -114,23 +135,30 @@ async function main() {
         console.log('Failed to obtain TranscriptionJobName...');
     }
     else {
-        let x = await createRecord(s3Uri, jobId);
+        let x = await createRecord(S3Uri, jobId);
+
+        console.log(`Deleting local file ${fileName}.. closing MongoDB Atlas connection...`)
 
         mongoose.connection.close();
 
-        console.log(x);        
+        fs.unlinkSync(fileName);
+        
+        console.log(x);
+
+        console.log('We are done...');
     }
 
-    // This does not return the transcription results.
+    // Note: This does not return the transcription results.
     //
     // Lamba Function created (in AWS) to wait for a CloudWatch event (this must also be set up)
     // When the transcription job is complete Transcribe issues an event that triggers our Lambda
     // function to send a callback to our server that the transcription is done (JobTitle included).
     // Using JobTitle we can continue to use AWS SDK to look up that JobTitle/ID and get results
-    // for storage in our waiting web-hook. -Lawrence
+    // for storage in our waiting web-hook. 
 }
 
 main();
+
 
 /*
 This is what the Lamba function sends to our https server on port 8051 for processing and clean-up
